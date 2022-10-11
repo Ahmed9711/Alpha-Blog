9bd074a157888be5f69b43b1e6798217b215e4f3
Ahmed9711 <ahmedzaitoon40@gmail.com>
2022-10-11 14:04:39 +0200

    Edit user profile added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,27 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Account has been updated"
+            redirect_to articles_path
+        else
+            render 'edit', status: :unprocessable_entity
+        end 
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up !"
             redirect_to articles_path
         else
-            render 'new'
+            render 'new', status: :unprocessable_entity
         end
     end
 
