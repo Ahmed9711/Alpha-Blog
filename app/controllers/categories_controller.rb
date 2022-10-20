933e3a31eac69a44b3e0ecd5b3783b588364edff
Ahmed9711 <ahmedzaitoon40@gmail.com>
2022-10-20 17:51:48 +0200

    Integration tests (Create and listing categories)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,11 @@ class CategoriesController < ApplicationController
     end
 
     def show
+        @category = Category.find(params[:id])
     end
 
     def index
+        @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
     def create
